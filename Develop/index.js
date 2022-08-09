ea00ef64656e86f1bdd112a3f132ef5971077439
Richard Caleb Lawver <clawver30@gmail.com>
2022-08-09 17:31:22 -0600

    added packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-LK;HASDGHKL;DGSHKL;GSDAHKLSDGAHLKS
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
